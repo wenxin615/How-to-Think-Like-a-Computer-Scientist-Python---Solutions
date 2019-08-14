4e2fc379c25580613aa3cdb2b1eb648f98026f0e
wenxin615 <53806103+wenxin615@users.noreply.github.com>
2019-08-14 11:46:17 +0800

    Create 5.1.19 Execises Q7.py

---
+++ 5.1.19 Execises Q7.py
@@ -0,0 +1,15 @@
+def reverse(words):
+    
+    for word in reversed(range(len(words))):
+        print(words[word],end="")
+    print("\n")
+reverse("happy")
+reverse("Python")
+reverse("")
+reverse("a")
+    
+    
+    
+    
+    
+    
