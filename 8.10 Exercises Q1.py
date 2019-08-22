67cdf374be1679f5cfeed44da0fcd80001d0c865
wenxin615 <53806103+wenxin615@users.noreply.github.com>
2019-08-22 09:50:29 +0800

    Create 8.10 Exercises Q1.py

---
+++ 8.10 Exercises Q1.py
@@ -0,0 +1,11 @@
+#Q1 (b)
+import calendar
+cal = calendar.TextCalendar(firstweekday=3)
+cal.pryear(2019)
+
+
+#Q1 (c)
+import calendar
+cal = calendar.TextCalendar(firstweekday=3)
+cal.prmonth(2019,5)
+
